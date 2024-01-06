58d5c8ab7a23765e6ee66e38f783fb87ac462ebb
Sotelo <miguel@space.bar>
2024-01-05 19:06:54 -0600

    First commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,14 @@
+/**
+ * Esta función me permite tomar una carta
+ * @param {Array<String>} deck arreglo de cartas - string
+ * @returns {Array<String>} retorna una carta del deck
+ */
+
+export const pedirCarta = (deck) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    return carta;
+}
